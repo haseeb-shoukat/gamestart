f2485dd3fcbc374892b1399c65fc79a944490c0e
haseeb-shoukat <haseebshoukat52@gmail.com>
2023-02-22 19:06:06 +0300

    Create Schema for Genre model

---
+++ models/genre.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+});
+
+// Virtual for game's URL
+GenreSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/genres/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Genre", GenreSchema);
