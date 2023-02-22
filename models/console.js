515e29479aa7b91e3981025da751b83ab6bcea14
haseeb-shoukat <haseebshoukat52@gmail.com>
2023-02-22 18:24:11 +0300

    Add description and in_stock fields

--- models/console.js
+++ models/console.js
@@ -5,6 +5,8 @@ const { DateTime } = require("luxon");
 const ConsoleSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
   company: { type: String, required: true, maxLength: 100 },
+  description: { type: String, maxLength: 2000, required: true },
+  in_stock: { type: Number, required: true, min: 0, max: 100000000 },
   price: { type: Number, required: true, min: 1, max: 100000000 },
   release_date: { type: Date },
 });
@@ -15,7 +17,7 @@ ConsoleSchema.virtual("url").get(function () {
   return `/consoles/${this._id}`;
 });
 
-ConsoleSchema.virtual("release_date").get(function () {
+ConsoleSchema.virtual("formatted_release_date").get(function () {
   return this.release_date
     ? DateTime.fromJSDate(this.release_date).toLocaleString(DateTime.DATE_MED)
     : "";
