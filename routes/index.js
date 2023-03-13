7815e6e03ec57944831a280c441a671feb6369cb
haseeb-shoukat <haseebshoukat52@gmail.com>
2023-03-13 20:51:02 +0300

    Load recent db items to index view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require("express");
 var router = express.Router();
+const Accessory = require("../models/accessory");
+const Console = require("../models/console");
+const Game = require("../models/game");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index");
+  async.parallel(
+    {
+      accessories(callback) {
+        Accessory.find().populate("console").limit(3).exec(callback);
+      },
+      consoles(callback) {
+        Console.find().limit(3).exec(callback);
+      },
+      games(callback) {
+        Game.find()
+          .populate("genre")
+          .populate("console")
+          .limit(3)
+          .exec(callback);
+      },
+    },
+    (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      res.render("index", {
+        recent_accessories: result.accessories,
+        recent_games: result.games,
+        recent_consoles: result.consoles,
+      });
+    }
+  );
 });
 
 module.exports = router;
