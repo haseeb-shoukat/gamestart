c76b743c7272a1405562a38ba86fa0ed232bb3e8
haseeb-shoukat <haseebshoukat52@gmail.com>
2023-03-31 00:11:09 +0500

    Get items for best sellers section

--- routes/index.js
+++ routes/index.js
@@ -9,28 +9,53 @@ const Game = require("../models/game");
 router.get("/", function (req, res, next) {
   async.parallel(
     {
-      accessories(callback) {
-        Accessory.find({}).populate("console").limit(3).exec(callback);
-      },
-      consoles(callback) {
-        Console.find({}).limit(2).exec(callback);
-      },
-      games(callback) {
-        Game.find({})
-          .populate("genre")
+      recent_accessories(callback) {
+        Accessory.find({})
           .populate("console")
+          .sort({ last_updated: -1 })
           .limit(3)
           .exec(callback);
       },
+      recent_consoles(callback) {
+        Console.find({}).limit(2).sort({ last_updated: -1 }).exec(callback);
+      },
+      recent_games(callback) {
+        Game.find({})
+          .populate("genre")
+          .populate("console")
+          .sort({ last_updated: -1 })
+          .limit(3)
+          .exec(callback);
+      },
+      best_accessories(callback) {
+        Accessory.find({}).limit(1).sort({ in_stock: 1 }).exec(callback);
+      },
+      best_consoles(callback) {
+        Console.find({}).limit(2).sort({ in_stock: 1 }).exec(callback);
+      },
+      best_games(callback) {
+        Game.find({}).limit(2).sort({ in_stock: 1 }).exec(callback);
+      },
     },
-    (err, { games, accessories, consoles }) => {
+    (err, result) => {
       if (err) {
         return next(err);
       }
       res.render("index", {
-        result: [...accessories, ...games, ...consoles].sort((a, b) => {
+        recent_items: [
+          ...result.recent_accessories,
+          ...result.recent_games,
+          ...result.recent_consoles,
+        ].sort((a, b) => {
           a.last_updated > b.last_updated ? -1 : 1;
         }), // Sort array to get most recent items
+        best_items: [
+          ...result.best_accessories,
+          ...result.best_games,
+          ...result.best_consoles,
+        ].sort((a, b) => {
+          a.in_stock < b.in_stock ? -1 : 1;
+        }), // Sort array to get items with lowest stock units,
       });
     }
   );
